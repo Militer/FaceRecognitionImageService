a47d192ea9119cf8cfe96159e69c4ea3df39ea9f
militer <gabi11a@yahoo.com>
2017-06-01 13:21:56 +0300

    Gateway now uses service registry.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,12 +2,14 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by militer on 19.03.2017.
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
